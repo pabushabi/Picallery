fccaf84e77506f4d2fb74984f5e33a25a0b645b5
Pavel Shalimov <pabushabi@mail.ru>
2018-03-05 21:57:42 +0900

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,19 @@
+document.write("Hello!" + "<br/>");
+document.write("Hi^^" + "<br/>");
+//alert("Hello World!");
+
+for (var i = 0; i < 10; i++)
+{
+    if (i % 10 == 1) {
+        document.write(i + "st element." + "<br/>");
+    }
+    else if (i % 10 == 2) {
+        document.write(i + "nd element." + "<br/>");
+    }
+    else if (i % 10 == 3) {
+        document.write(i + "rd element." + "<br/>");
+    }
+    else document.write(i + "th element." + "<br/>")
+}
+
+window.ClipboardData();
