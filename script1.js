a683306f96de98e9a3210020dbef340c49b88e3d
Pavel Shalimov <pabushabi@mail.ru>
2018-03-08 13:01:10 +0900

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,18 +1,73 @@
-document.write("Hello!" + "<br/>");
-document.write("Hi^^" + "<br/>");
-//alert("Hello World!");
+console.log("Hi^^");
 
 for (var i = 0; i < 10; i++)
 {
-    if (i % 10 == 1) {
-        document.write(i + "st element." + "<br/>");
+    if (i % 10 === 1)
+        console.log(i + "st element.");
+    else if (i % 10 === 2)
+        console.log(i + "nd element.");
+    else if (i % 10 === 3)
+        console.log(i + "rd element.");
+    else
+        console.log(i + "th element.")
+}
+console.log("Random number: ",Math.round(Math.random() * 10));
+var arr = ["a", 1, 3.14];
+for (var i = 0; i < arr.length; i++)
+    console.log(arr[i]);
+console.log(arr);
+
+function logIn() {
+    var element = document.getElementById("login");
+    var email = document.forms["logIn"]["E-mail"].value;
+    var pass = document.forms["logIn"]["Password"].value;
+    if (email === "") {
+        alert("Enter valid E-mail!");
+        return false;
     }
-    else if (i % 10 == 2) {
-        document.write(i + "nd element." + "<br/>");
+    else {
+        element.innerHTML = "You are logged in";
     }
-    else if (i % 10 == 3) {
-        document.write(i + "rd element." + "<br/>");
+    if (pass === ""){
+        alert("Enter valid password!");
+        return false;
     }
-    else document.write(i + "th element." + "<br/>")
 }
 
+var title = document.getElementById("title");
+console.log(title);
+title.innerHTML = "1 new message";
+
+function hack() {
+    setTimeout(h1, 1000);
+}
+
+function h1() {
+    var pic = document.getElementById("Picallery");
+    pic.innerHTML = "HACKED";
+
+}
+
+function resize() {
+    var pic = document.getElementsByClassName("gallery");
+    for (var k = 0; k < 5; k++)
+        pic[k].src = "resources/5.jpg"
+}
+
+function mouseInFoot() {
+    var pic = document.getElementsByClassName("foot");
+    pic[0].style.color = "red";
+}
+function mouseLeaveFoot() {
+    var pic = document.getElementsByClassName("foot");
+    pic[0].style.color = "black";
+}
+
+function mouseEnterUp() {
+    var pic = document.getElementById("up");
+    pic.src = "resources/upMoused.jpg";
+}
+function mouseLeaveUp() {
+    var pic = document.getElementById("up");
+    pic.src = "resources/up.jpg";
+}
